31eb1689b96b920ca2bfb8ba7daa3f9dde150e20
reekyree <bl.luster.32@gmail.com>
2023-07-25 23:23:54 -0500

    Created if statement for writing a file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # A terminal password manager. 
 
 # TODO
+# Move save func to the generation side of the if/else statement
+# Create funcs
 # Create a file to write passwords to.
 # Ask the user if they want to save a password.
 # Save the password to a username in a dictionary.
@@ -12,6 +14,28 @@ answer = input("Would you like to generate a new pasword? (y/n) ")
 
 if answer.lower() == 'y':
     password = generator.main()
+    save = input("Would you like to save the password? ")
+
+    if save.lower() == 'y':
+        pass_file = open("passwords.txt", "w")
+        pass_file.write(password)
+        print("Your new password has been saved.")
+        pass_file.close()
+    else:
+        print("Goodbye.")
+
 else:
     print("Then we are done here.")
+
+save = input("Would you like to save the password? ")
+
+if save.lower() == 'y':
+    pass_file = open("passwords.txt", "w")
+    pass_file.write(password)
+    print("Your new password has been saved.")
+    pass_file.close()
+else:
+    print("Goodbye.")
+
+
 
