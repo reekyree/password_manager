bb85ddc7517e3b11f7c7ca1ddea00526cd9a545c
reekyree <bl.luster.32@gmail.com>
2023-07-25 23:47:38 -0500

    Made code more readable

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # A terminal password manager. 
 
 # TODO
-# Move save func to the generation side of the if/else statement
 # Create funcs
-# Create a file to write passwords to.
-# Ask the user if they want to save a password.
 # Save the password to a username in a dictionary.
+# Create a function to display all passwords in the txt file.
+
 
 import generator
 
@@ -25,9 +24,16 @@ def savePass(password):
 answer = input("Would you like to generate a new pasword? (y/n) ")
 
 if answer.lower() == 'y':
-    password = generator.main()
-    save = input("Would you like to save the password? ")
-    savePass(password)
 
-else:
-    print("Then we are done here.")
+    password = generator.main()
+    
+    save = input("Would you like to save the password? ")
+    
+    if save.lower() == 'y':
+
+        savePass(password)
+
+    else:
+        
+        print("Then we are done here.")
+
