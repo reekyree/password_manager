b8ad62882c3df6901ee26bdeff7b9efc92187dc9
reekyree <bl.luster.32@gmail.com>
2023-07-28 07:32:01 -0500

    Seperated the program into functions. Tests still needed

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # A terminal password manager. 
 
 # TODO
+# Create a 'menu' function.
 # Create funcs
 # Save the password to a username in a dictionary.
 # Create a function to display all passwords in the txt file.
 
 
 import generator, sys
+
+
+def showMenu():
+    print("(1) Generate a new password")
+    print("(2) ")
 
 
 def savePass(password):
@@ -25,24 +31,20 @@ def displayAll():
     print("Coming soon!")
 
 def generatePass():
-    # Generate a new password.
-    print("Coming soon!")
-
-# Ask whether the user wants to generate a password.
-answer = input("Would you like to generate a new pasword? (y/n) ")
-
-if answer.lower() == 'y':
 
     password = generator.main()
-    
-    save = input("Would you like to save the password? ")
-    
-    if save.lower() == 'y':
+    return password
 
-        savePass(password)
+
     
-    else:
-        
-        print("Then we are done here.")
-        sys.exit()
+def main():
+
+    # Main program function
+    on = True
+
+    while on:
+        showMenu()
+
+        action = input("What would you like to do? ")
+
 
