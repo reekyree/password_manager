c9e0dfaa81ff8dc7b79fa60951dd1da1c5e54172
reekyree <bl.luster.32@gmail.com>
2026-04-21 13:46:22 -0500

    Rebuild of the password creation from the ground up started. Currently unusable in its current state. More detailed messages to come

--- generator.py
+++ generator.py
@@ -3,95 +3,55 @@
 # The user can specify the number of characters used.
 
 # TODO
-# getLength needs error handling for non int and 0 inputs.
-# addChar needs error handling for non int
-# addSpecial needs handling for non int
-# Specified password length does not output according to length limit if the user
-# enters more characters than initially specified.
+# Ask user to specifiy desired # of alpha, digits, and special
+# Show user password character length after getting parameters
+# Create password based on parameters
+# Display password in non-list format
 
-
-import secrets, string, random # Keeping random so program doesn't break for now, switching to secrets and will be dropping random module entirely once changes are made
-
-# password variable is global, move it to local
- 
+import secrets, string, sys   
 
 def createPass():
-    
+   
+    # Starting variables
     password = []
+    alpha = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+    total_alpha = 0
+    total_digits = 0
+    total_special = 0
 
-    # Update this function to be createPass()
-    # Get the password length.
     while True:
-        try: 
-            length = int(input("Password length (number): "))
-        except ValueError:
-            print("Please enter a valid number.")
-    
-        # Get # of integers in password
         try:
-            amtInt = int(input("Min. # of integers (0 if none): "))
-            if amtInt > 0:
-                digits = string.digits
-                intpass = secrets.choice(digits)
-            else:
-                print("You are adding (0) numbers.")
+            total_alpha = int(input("# of letters: "))
         except ValueError:
-            print("Please enter a valid number.")
+            print("Enter a valid number.")
 
-        # Get # of alphabetic characters in password 
-        try: 
-            amtAlpha = int(input("Min. # of letters (0 if none): "))
-            if amtAlpha > 0:
-                alphabet = string.ascii_letters
-                alphapass = secrets.choice(alphabet)
-            else:
-                print("You are adding (0) numbers.")
-        except ValueError:
-            print("Please enter a valid number.")
+        total_digits = int(input("# of digits: "))
+        total_special = int(input("# of special: "))
+        length = total_alpha + total_digits + total_special
+        choice = input(f"password will be {length} characters long. y/n: ")
+
+        if choice.lower() == 'y':
+
+            while len(password) < length:
+                add_alpha = secrets.choice(alpha)
+                password.append(add_alpha)
+   
+    # shuffle password (replaces shufflePass func) 
+    secrets.SystemRandom().shuffle(password)
     
-        # Get # of special characters 
-        try:
-            addSpecial = int(input("Min. # of special characters (0 if none): "))
-            if addSpecial > 0:
-                special = secrets.choice(string.punctuation)
-                password.append(special)
-            else:
-                print("You are adding (0) special characters.")
-        except ValueError:
-            print("Please enter a valid number.")
-
-    while len(password) < length:
-        char = secrets.choice(string.ascii_letters)
-        password.append(char)
-
-    passString = ''.join(str(i) for i in password)
-
-    return passString
-
-
-
-    #def shufflePass(password):
-    # Randomize the password.
-    #secrets.SystemRandom().shuffle(password)
-
-    #def displayPass(password):
+    return password
+        
+def displayPass(password):
     # Display the password in a non-list format.
-    #for char in password:
-    #print(char, end='')
-
+    for char in password:
+        print(char, end='')
 
 def main():
-
     # This logic needs to be changed to reflect the removal of the other functions 
-
     password = createPass()
-
-    #shufflePass(password)
-
-    #displayPass(password)
-    
-    print(password)
+    displayPass(password)
 
 if __name__ == "__main__":
     main()
-    
